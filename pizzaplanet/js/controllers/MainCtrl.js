cd454696254ae388e8ed9ee50565739a9a582fc7
Chris Brody <chrisbrody@codifyacademy.com>
2015-08-27 10:03:17 -0400

    created pizza planet app

---
+++ pizzaplanet/js/controllers/MainCtrl.js
@@ -0,0 +1,57 @@
+app.controller('MainCtrl', ['$scope', function ($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzalrella, tomatoes, basil, balsamic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+
+  $scope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marinara sauce, mozzarella, fresh basil.',
+      price: 6.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+      price: 7.95
+    },
+    {
+      name: 'Rustica',
+      description: 'Marinara sauce, kalamata olives, roasted garlic, fresh oregano.',
+      price: 8.95
+    }
+  ];
+
+  $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Served with marinara sauce.',
+      price: 4.95
+    },
+    {
+      name: 'Soupp of the day',
+      description: 'Take a chance.',
+      price: 4.95
+    },
+    {
+      name: 'Buffalo Wings',
+      description: 'Feel the power of wings.',
+      price: 6.95
+    }
+  ];
+}]);
